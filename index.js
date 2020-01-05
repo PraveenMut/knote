a220ef7e0b6f39a2f6d9ce20ff689eb5f37d0893
praveen <praveen.mut@gmail.com>
2020-01-05 13:45:53 +1100

    Add in MinIO as an independent object storage

--- index.js
+++ index.js
@@ -6,11 +6,39 @@ const MongoClient = require('mongodb').MongoClient;
 const express = require('express');
 const multer = require('multer');
 const marked = require('marked');
+const minio = require('minio');
 
+// instantiation of dependencies
 const app = express();
 const port = process.env.PORT || 3000;
 const mongoURL = process.env.MONGO_URL || 'mongodb://localhost:27017/dev';
+const minioHost = process.env.MINO_HOST || '127.0.0.1';
+const minioBucket = 'image-storage';
 
+// initalise MinIO
+const initialiseMinIO = async () => {
+  console.log('Initalising MinIO Object Storage');
+  const client = new minio.Client({
+    endPoint: minioHost,
+    port: 9000,
+    useSSL: false,
+    accessKey: process.env.MINIO_ACCESS_KEY,
+    secretKey: process.env.MINIO_SECRET_KEY,
+  });
+  let success = false;
+  while(!success) {
+    try {
+      if(!(await client.bucketExists(minioBucket))) {
+        await client.makeBucket(minioBucket);
+      };
+    } catch(e) {
+      console.log('Could not load MinIO, waiting for 1 second before reattempt...\nException details:', e);
+      await new Promise(resolve => setTimeout(resolve, 1000))
+    }
+  }
+  console.log('MinIO has been successfully initalised');
+  return client;
+}
 
 async function initialiseMongo() {
   console.log('Initialising MongoDB...');
@@ -31,6 +59,7 @@ async function initialiseMongo() {
 const start = async () => {
   // start and listen for MongoDB
   const db = await initialiseMongo();
+  const minio = await initialiseMinIO();
   // use pug as templating engine
   app.set('view engine', 'pug');
   app.set('views', path.join(__dirname, 'views'));
@@ -40,18 +69,23 @@ const start = async () => {
     console.log(`App listening on http://localhost:${port}`);
   })
 
-  // render index page
+  // render pages
   app.get('/', async (req, res) => res.render('index', { notes: await retrieveNotes(db) }));
+  app.get('/img/:name', async(req, res) => {
+    const stream = await minio.getObject(minioBucket, decodeURIComponent(req.params.name));
+    stream.pipe(res);
+  });
 
   app.post(
     '/note',
-    multer({ dest: path.join(__dirname, 'public/uploads/') }).single('image'),
+    multer({ storage: multer.memoryStorage() }).single('image'),
     async (req, res) => {
       if (!req.body.upload && req.body.description) {
         await saveNote(db, { description: req.body.description });
         res.redirect('/');
       } else if (req.body.upload && req.file) {
-        const link = `/uploads/${encodeURIComponent(req.file.filename)}`;
+        await minio.putObject(minioBucket, req.file.originalname, req.file.buffer);
+        const link = `/img/${encodeURIComponent(req.file.filename)}`;
         res.render('index', {
           content: `${req.body.description} ![](${link})`,
           notes: await retrieveNotes(db),
